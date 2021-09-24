ec625e794ed4c810d5fcd6da889c5d530987c8e2
l33th <l33th@protonmail.ch>
2021-09-24 16:51:37 +0200

    Functions

---
+++ 4-Functions/ArrowFunction.ts
@@ -0,0 +1,5 @@
+const getFullName = (name: string, surname: string, age: number) => {
+    return name + " " + surname + " of age " + age;
+}
+
+console.log(getFullName("Web", "Developer", 250));
---
+++ 4-Functions/DefineAFunction.ts
@@ -0,0 +1,5 @@
+const devFullName = (name: string, surname: string, age: number): string => {
+    return name + " " + surname + " of age " + age;
+}
+
+console.log(devFullName("Web", "Developer", 250));
