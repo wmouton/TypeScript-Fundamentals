afdd67abdf5b754355e6e810b577be09c9633f78
l33th <l33th@protonmail.ch>
2021-09-26 07:59:34 +0200

    Types and Unions

---
+++ 6-Types-And-Unions/unionOperator.ts
@@ -0,0 +1,14 @@
+interface UserInterface {
+    name: string
+    surname: string
+}
+
+let username: string = "Developer";
+
+let pageNumber: string | number = "1";
+
+let errorMessage: string | null = null;
+
+let user: UserInterface | null = null;
+
+let someProp: string | number | null | undefined | string[] | object
